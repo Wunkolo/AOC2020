2a44305d381b5ccaa84114aa69419a29cd320abf
Wunkolo <Wunkolo@gmail.com>
2020-12-04 08:22:38 -0800

    Optimize Day 2 solution
    Use integer based hash map of passport attributes rather than string
    comparisons. Emit compile-tile identifier for 3-character string types.

--- Day 04/main.cpp
+++ Day 04/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sstream>
+#include <string_view>
 #include <algorithm>
 #include <unordered_map>
 #include <unordered_set>
@@ -9,44 +10,65 @@ template<std::uintmax_t Min, std::uintmax_t Max>
 bool Between(std::uintmax_t Number){return (Min <= Number) && (Number <= Max);}
 
 template<std::uintmax_t Min, std::uintmax_t Max, std::uintmax_t Digits, std::uintmax_t Base = 10>
-bool ValidNumber(const std::string& String)
+bool ValidNumber(std::string_view String)
 {
 	if(String.length() != Digits) return false;
 	std::uintmax_t Number;
 	const std::from_chars_result ParseResult = std::from_chars(
 		String.data(), String.data() + Digits, Number, Base
-	);
-	if(ParseResult.ec != std::errc()) return false;
+	); if(ParseResult.ec != std::errc()) return false;
 	return Between<Min, Max>(Number);
 }
 
-bool ValidHeight(const std::string& String)
+bool ValidHeight(std::string_view String)
 {
 	std::uintmax_t Number;
 	const std::from_chars_result ParseResult = std::from_chars(
 		String.data(), String.data() + String.length(), Number
-	);
-	if(ParseResult.ec != std::errc()) return false;
+	); if(ParseResult.ec != std::errc()) return false;
 	const std::string_view Unit(ParseResult.ptr);
 	if( Unit == "cm" )		return Between<150, 193>(Number);
 	else if( Unit == "in")	return Between<59, 76>(Number);
 	return false;
 }
 
-const std::unordered_set<std::string> EyeColors{{"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}};
-const std::unordered_map<const char*, bool(*)(const std::string&)> RequiredFields{
-	{ "byr", ValidNumber<1920, 2002, 4>},
-	{ "iyr", ValidNumber<2010, 2020, 4>},
-	{ "eyr", ValidNumber<2020, 2030, 4>},
-	{ "hgt", ValidHeight},
-	{ "hcl", [](const std::string& String) -> bool{return String[0] == '#' && ValidNumber<0, 0xFFFFFF, 6, 16>(String.substr(1));}},
-	{ "ecl", [](const std::string& String) -> bool{return EyeColors.count(String);} },
-	{ "pid", ValidNumber<0, 999999999, 9>}
+bool ValidHairColor(std::string_view String)
+{
+	return String[0] == '#' && ValidNumber<0, 0xFFFFFF, 6, 16>(String.substr(1));
+}
+
+template<std::size_t N>
+inline constexpr std::uint32_t Tag( const char (&TagString)[N])
+{
+	static_assert(N == 4, "Tag must be 3 characters");
+	return (TagString[2] << 16) | (TagString[1] <<  8) | (TagString[0]);
+}
+
+bool ValidEyeColor(std::string_view String)
+{
+	if( String.length() != 3 ) return false;
+	switch( *(std::uint32_t*)String.data() )
+	{
+		case Tag("amb"): case Tag("blu"): case Tag("brn"): case Tag("gry"):
+		case Tag("grn"): case Tag("hzl"): case Tag("oth"):
+			return true;
+	}
+	return false;
+}
+
+const static std::unordered_map<std::uint32_t, bool(*)(std::string_view)> RequiredFields{
+	{ Tag("byr"), ValidNumber<1920, 2002, 4>},
+	{ Tag("iyr"), ValidNumber<2010, 2020, 4>},
+	{ Tag("eyr"), ValidNumber<2020, 2030, 4>},
+	{ Tag("hgt"), ValidHeight},
+	{ Tag("hcl"), ValidHairColor},
+	{ Tag("ecl"), ValidEyeColor },
+	{ Tag("pid"), ValidNumber<0, 999999999, 9>}
 };
 
-int main(int argc, char *argv[])
+int main()
 {
-	std::unordered_map<std::string, std::string> CurPassport;
+	std::unordered_map<std::uint32_t, std::string> CurPassport;
 	std::size_t Part1{}, Part2{}; std::string CurLine;
 	while( std::getline(std::cin, CurLine) )
 	{
@@ -73,7 +95,7 @@ int main(int argc, char *argv[])
 		while( LineStream >> CurAttribute)
 		{
 			const std::size_t Colon = CurAttribute.find_first_of(':');
-			CurPassport[CurAttribute.substr(0, Colon)] = CurAttribute.substr(Colon + 1);
+			CurPassport[*(std::uint32_t*)CurAttribute.substr(0, Colon).data()] = CurAttribute.substr(Colon + 1);
 		}
 	}
 	std::cout << Part1 << std::endl; std::cout << Part2 << std::endl;
