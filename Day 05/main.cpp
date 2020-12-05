07d44fc85fe9d8be5b7214a0cc2627fd9e28d59a
Wunkolo <Wunkolo@gmail.com>
2020-12-05 01:32:00 -0800

    Refactor Day 5 solution
    Super simplified now

--- Day 05/main.cpp
+++ Day 05/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 
-// XOR of all numbers from [1,N]
-// https://oeis.org/A003815
-inline std::uintmax_t SigmaXor(std::uintmax_t N)
+inline std::uintmax_t XorTo(std::uintmax_t N)
 {
 	return (N & (N << 1 & 0b11) - 1) ^ (N >> 1 & 0b1);
 }
@@ -10,20 +8,13 @@ inline std::uintmax_t SigmaXor(std::uintmax_t N)
 int main()
 {
 	std::uintmax_t Min{~0ull}, Max{}, Missing{}; std::string CurLine;
-	std::ios_base::sync_with_stdio(false); std::cin.tie(nullptr);
-	while( std::getline(std::cin, CurLine) )
+	while(std::getline(std::cin, CurLine))
 	{
 		std::uintmax_t SeatID{};
-		for( std::size_t i = 0; i < CurLine.length(); ++i )
-		{
-			SeatID <<= 1;
-			if(CurLine[i]=='B' || CurLine[i]=='R') SeatID |= 1;
-		}
+		for( auto Digit : CurLine ) (SeatID <<= 1) |= (Digit=='B' || Digit=='R');
 		Missing ^= SeatID;
 		Max = std::max(Max, SeatID); Min = std::min(Min, SeatID);
 	}
-	// Missing is the XOR of all processed seats, except our own
-	// So we XOR all the seats between Min and Max, leaving only one seat left
-	Missing ^= SigmaXor(Min-1) ^ SigmaXor(Max);
+	Missing ^= XorTo(Min-1) ^ XorTo(Max);
 	std::cout << Max << '\n' << Missing << std::endl;
 }
