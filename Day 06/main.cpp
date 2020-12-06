5fe4bcabb156ccc2a85e6fbb5c6ba1cb672e8a24
Wunkolo <Wunkolo@gmail.com>
2020-12-06 01:20:39 -0800

    Refactor Day 6 for C++20 std::popcount

--- Day 06/main.cpp
+++ Day 06/main.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
+#include <bit>
 
 int main()
 {
 	std::uint32_t Part1{}, Part2{}, GroupMask{}, QuestionMask{~0u};
 	std::string CurLine;
-	std::ios_base::sync_with_stdio(false); std::cin.tie(nullptr);
 	while( std::cin )
 	{
-		std::getline(std::cin, CurLine);
-		if( !CurLine.length() && GroupMask || !std::cin )
+		if( CurLine.clear(), std::getline(std::cin, CurLine); CurLine.empty())
 		{
-			Part1 += __builtin_popcount(GroupMask);
-			Part2 += __builtin_popcount(GroupMask & QuestionMask);
+			Part1 += std::popcount(GroupMask);
+			Part2 += std::popcount(GroupMask & QuestionMask);
 			GroupMask = 0; QuestionMask = ~0u;
-			continue;
 		}
-		std::uint32_t CurPerson{};
-		for( const auto& Char : CurLine ) CurPerson |= 1 << (Char - 'a');
-		GroupMask |= CurPerson; QuestionMask &= CurPerson;
+		else
+		{
+			std::uint32_t CurPerson{};
+			for( char Char : CurLine ) CurPerson |= 1 << (Char - 'a');
+			GroupMask |= CurPerson; QuestionMask &= CurPerson;
+		}
 	}
 	std::cout << Part1 << '\n' << Part2 << std::endl;
 }
