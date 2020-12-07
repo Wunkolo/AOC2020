0f6b5bcaa4b87168ca2c4f441de54f05cd02095a
Wunkolo <Wunkolo@gmail.com>
2020-12-06 22:17:42 -0800

    Add Day 7 Solution
    Not my sexiest code, but whatever.

---
+++ Day 07/main.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+
+using ContainedT =
+	std::unordered_map<
+		// Bag Color
+		std::string,
+		// Other bags this bag Contains + Count
+		std::unordered_map<std::string, std::size_t>
+	>;
+
+using ContainedInT =
+	std::unordered_map<
+		// Bag Color
+		std::string,
+		// Bags that contain this bag
+		std::unordered_set<std::string>
+	>;
+
+std::uintmax_t Check( const ContainedInT& ContainedIn, const std::string& Color )
+{
+	std::unordered_set<std::string> ContainedColor;
+	std::stack<std::string> Queue;
+	Queue.push(Color);
+
+	while( Queue.size() )
+	{
+		const std::string CurColor = Queue.top(); Queue.pop();
+		if( ContainedIn.count(CurColor) )
+		{
+			for(auto Container : ContainedIn.at(CurColor))
+			{
+				ContainedColor.insert(Container);
+				Queue.push(Container);
+			}
+		}
+	}
+	return ContainedColor.size();
+}
+
+std::uintmax_t BagCount( const ContainedT& Contained, const std::string& Color )
+{
+	std::uintmax_t Total = 0;
+	if( Contained.count(Color) )
+	{
+		for(auto Rule : Contained.at(Color))
+		{
+			Total += Rule.second * (BagCount(Contained, Rule.first) + 1);
+		}
+	}
+	return Total;
+}
+
+int main()
+{
+	std::string CurLine{}, Cull{};
+	ContainedT Contained; ContainedInT ContainedIn;
+	while( std::getline(std::cin, CurLine) )
+	{
+		std::stringstream LineStream(CurLine);
+		std::string Adjective{}, Color{};
+		LineStream >> Adjective >> Color >> Cull >> Cull;
+		while( LineStream )
+		{
+			std::uintmax_t Count;
+			std::string InnerAdjective{}, InnerColor{};
+			if( LineStream >> Count >> InnerAdjective >> InnerColor >> Cull )
+			{
+				ContainedIn[InnerAdjective + InnerColor].insert(Adjective + Color);
+				Contained[Adjective + Color][InnerAdjective + InnerColor] = Count;
+			}
+		}
+	}
+
+	std::cout
+		<< Check(ContainedIn, "shinygold")
+		<< '\n'
+		<< BagCount(Contained, "shinygold")
+		<< std::endl;
+}
