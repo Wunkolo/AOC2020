bbc97c3556191894753e06f84dcc1b75a5dc3e74
Wunkolo <Wunkolo@gmail.com>
2020-12-07 22:23:44 -0800

    Refactor Day 8 solution

--- Day 08/main.cpp
+++ Day 08/main.cpp
@@ -13,65 +13,47 @@ struct Instruction
 {
 	enum OpCode : std::uint32_t
 	{
-		Acc	= Opcode("acc"),
-		Jmp	= Opcode("jmp"),
-		Nop	= Opcode("nop")
-	} Operation;
+		Acc = Opcode("acc"), Jmp = Opcode("jmp"), Nop = Opcode("nop")
+	} Op;
 	std::intmax_t Operand;
 };
 
 bool Execute(const std::vector<Instruction>& Program, std::intmax_t& Result)
 {
-	std::vector<bool> Visited(Program.size(), false);
-	std::intmax_t Accumulator = 0;
-	std::uintmax_t ProgramCounter = 0;
+	std::vector<bool> Visited(Program.size());
+	Result = 0; std::uintmax_t ProgramCounter{};
 
-	while(Visited[ProgramCounter] == false && ProgramCounter < Program.size())
+	while( Visited[ProgramCounter] == false && ProgramCounter < Program.size() )
 	{
 		const Instruction& CurInstruction = Program[ProgramCounter];
 		Visited[ProgramCounter] = true;
-		switch(CurInstruction.Operation)
+		switch(CurInstruction.Op)
 		{
-			case Instruction::OpCode::Acc:
-			{
-				Accumulator += CurInstruction.Operand;
-				++ProgramCounter;
-				break;
-			}
-			case Instruction::OpCode::Jmp:
-			{
-				ProgramCounter += CurInstruction.Operand;
-				break;
-			}
-			case Instruction::OpCode::Nop:
-			{
-				++ProgramCounter;
-				break;
-			}
+			case Instruction::OpCode::Acc: Result += CurInstruction.Operand;
+			case Instruction::OpCode::Nop: ++ProgramCounter; break;
+			case Instruction::OpCode::Jmp: ProgramCounter += CurInstruction.Operand; break;
 		}
 	}
-	Result = Accumulator;
 	return ProgramCounter >= Program.size() ? true : false;
 }
 
 std::intmax_t Part2(const std::vector<Instruction>& Program)
 {
-	std::intmax_t CurResult;
-	std::vector<Instruction> NewProgram = Program;
+	std::intmax_t CurResult; std::vector<Instruction> NewProgram = Program;
 	for(std::size_t i = 0; i < Program.size(); ++i)
 	{
 		const Instruction& CurInstruction = Program[i];
 		Instruction::OpCode NewOpCode = {};
-		switch(CurInstruction.Operation)
+		switch(CurInstruction.Op)
 		{
 			case Instruction::OpCode::Jmp: NewOpCode = Instruction::OpCode::Nop; break;
 			case Instruction::OpCode::Nop: NewOpCode = Instruction::OpCode::Jmp; break;
 		}
 		if(NewOpCode)
 		{
-			NewProgram[i].Operation = NewOpCode;
+			NewProgram[i].Op = NewOpCode;
 			if(Execute(NewProgram, CurResult)) return CurResult;
-			NewProgram[i].Operation = CurInstruction.Operation;
+			NewProgram[i].Op = CurInstruction.Op;
 		}
 	}
 	return 0;
@@ -79,10 +61,10 @@ std::intmax_t Part2(const std::vector<Instruction>& Program)
 
 int main()
 {
-	char Operation[4]; std::intmax_t Operand;
+	char Op[4]; std::intmax_t Operand, Part1;
 	std::vector<Instruction> Program;
-	while( std::scanf(" %4s %ju", Operation, &Operand) == 2 )
-		Program.push_back({*(Instruction::OpCode*)Operation, Operand});
-	std::intmax_t Part1; Execute(Program, Part1);
+	while( std::scanf(" %3s %ju", Op, &Operand) == 2 )
+		Program.push_back({*(Instruction::OpCode*)Op, Operand});
+	Execute(Program, Part1);
 	std::cout << Part1 << '\n' << Part2(Program) << std::endl;
 }
