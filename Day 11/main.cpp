df288082322b6181668f9c91fd98efd5751144a4
Wunkolo <Wunkolo@gmail.com>
2020-12-11 10:56:40 -0800

    Optimize Day 11 solution

--- Day 11/main.cpp
+++ Day 11/main.cpp
@@ -5,40 +5,33 @@
 #include <tuple>
 #include <unordered_set>
 
-std::uint64_t EncodePoint(std::uint32_t X, std::uint32_t Y)
+std::uint32_t EncodePoint(std::uint16_t X, std::uint16_t Y)
 {
-	return (std::uint64_t(Y) << 32) | X;
+	return (std::uint32_t(Y) << 16) | X;
 }
 
-std::tuple<std::uint32_t, std::uint32_t> DecodePoint(std::uint64_t Point)
+std::tuple<std::uint16_t, std::uint16_t> DecodePoint(std::uint32_t Point)
 {
-	return {std::uint32_t(Point), std::uint32_t(Point >> 32)};
+	return {std::uint16_t(Point), std::uint16_t(Point >> 16)};
 }
-constexpr std::array<std::tuple<std::int32_t, std::int32_t>, 8> Offsets = {{
-	{ 0,  1},
-	{ 0, -1},
-	{ 1,  0},
-	{-1,  0},
-	{ 1,  1},
-	{-1, -1},
-	{ 1, -1},
-	{-1,  1}
+constexpr std::array<std::tuple<std::int8_t, std::int8_t>, 8> Offsets = {{
+	{ 0,  1},{ 0, -1},{ 1,  0},{-1,  0},{ 1,  1},{-1, -1},{ 1, -1},{-1,  1}
 }};
 
 std::uintmax_t Simulate(
-	std::unordered_set<std::uint64_t> EmptySeats,
-	std::unordered_set<std::uint64_t> OccupiedSeats,
+	std::unordered_set<std::uint32_t> EmptySeats,
+	std::unordered_set<std::uint32_t> OccupiedSeats,
 	std::uintmax_t SightRadius,
 	std::uintmax_t EmptySeatTolerance
 )
 {
 	
-	std::uint32_t Changed{};
+	std::uint16_t Changed{};
+	std::unordered_set<std::uint32_t> NewEmptySeats, NewOccupiedSeats;
 	do
 	{
 		Changed = 0;
-		std::unordered_set<std::uint64_t> NewEmptySeats;
-		std::unordered_set<std::uint64_t> NewOccupiedSeats;
+		NewEmptySeats.clear(); NewOccupiedSeats.clear();
 		for(const auto EmptySeat : EmptySeats)
 		{
 			const auto [SeatX, SeatY] = DecodePoint(EmptySeat);
@@ -57,6 +50,7 @@ std::uintmax_t Simulate(
 					SightHit |= EmptySeats.count(TestPoint);
 					if(SightHit) break;
 				}
+				if(OccupiedNeighbors) break; // Optimization
 			}
 
 			if(OccupiedNeighbors == 0)
@@ -81,11 +75,11 @@ std::uintmax_t Simulate(
 					SightHit |= EmptySeats.count(TestPoint);
 					if(SightHit) break;
 				}
-			}
-
-			if(OccupiedNeighbors >= EmptySeatTolerance)
-			{
-				NewEmptySeats.insert(OccupiedSeat); ++Changed;
+				if(OccupiedNeighbors >= EmptySeatTolerance)
+				{
+					NewEmptySeats.insert(OccupiedSeat); ++Changed;
+					break;
+				}
 			}
 		}
 		for(const auto Seat : NewOccupiedSeats){    EmptySeats.erase(Seat); OccupiedSeats.insert(Seat);}
@@ -96,23 +90,22 @@ std::uintmax_t Simulate(
 
 int main()
 {
-	std::unordered_set<std::uint64_t> EmptySeats;
-	std::unordered_set<std::uint64_t> OccupiedSeats;
+	std::unordered_set<std::uint32_t> EmptySeats;
+	std::unordered_set<std::uint32_t> OccupiedSeats;
 	std::string CurLine;
 
-	std::uint32_t Y = 0;
+	std::uint16_t Y{}, MaxDim{};
 	while(std::getline(std::cin, CurLine))
 	{
-		for(std::uint32_t X = 0; X < CurLine.size(); ++X)
+		for(std::uint16_t X = 0; X < CurLine.size(); ++X)
 		{
-			switch(CurLine[X])
-			{
-				case 'L': EmptySeats.insert(EncodePoint(X,Y)); break;
-			}
+			if(CurLine[X] == 'L') EmptySeats.insert(EncodePoint(X,Y));
+			MaxDim = std::max(MaxDim, X);
 		}
 		++Y;
+		MaxDim = std::max(MaxDim, Y);
 	}
 
-	std::cout << Simulate(EmptySeats, OccupiedSeats, 1, 4) << std::endl;
-	std::cout << Simulate(EmptySeats, OccupiedSeats, Y, 5) << std::endl;
+	std::cout << Simulate(EmptySeats, OccupiedSeats,      1, 4) << std::endl;
+	std::cout << Simulate(EmptySeats, OccupiedSeats, MaxDim, 5) << std::endl;
 }
