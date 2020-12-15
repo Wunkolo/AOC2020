07b1eb2e03767cfc1043137859b5a83876224e61
Wunkolo <Wunkolo@gmail.com>
2020-12-14 22:03:28 -0800

    Add Day 15 optimization note
    Gonna try and tighten this code up later.
    I don't feel good doing a '30000000' brute-force.
    Something-something fibonacci.

--- Day 15/main.cpp
+++ Day 15/main.cpp
@@ -5,8 +5,12 @@
 #include <vector>
 #include <unordered_map>
 
+// struct Ring2{}
+
 int main()
 {
+	// Todo, Hashmap of 2-element circular buffers?
+	// std::unordered_map<std::uintmax_t, Ring2> SaidNumbers;
 	std::unordered_map<std::uintmax_t, std::vector<std::intmax_t>> SaidNumbers;
 	std::uintmax_t PrevNumber{}, CurTurn{};
 	std::string CurToken;
@@ -17,18 +21,18 @@ int main()
 		SaidNumbers[PrevNumber].push_back(CurTurn);
 	}
 
-	++CurTurn;
+	++CurTurn; // 1-indexed -_-
 	for(; true; ++CurTurn)
 	{
 		std::uintmax_t CurNumber{};
 		if(SaidNumbers.count(PrevNumber) && SaidNumbers[PrevNumber].size() > 1)
 		{
+			// Ring2 Sum-absolute-difference?
 			CurNumber = std::abs(*SaidNumbers[PrevNumber].rbegin() - *(SaidNumbers[PrevNumber].rbegin() + 1));
 		}
+		// Push to Ring2 here
 		SaidNumbers[CurNumber].push_back(CurTurn);
 		PrevNumber = CurNumber;
-		//if(CurTurn == 2021)
-		//std::cout << CurNumber << std::endl;
 		switch(CurTurn)
 		{
 			case 2020		:{ std::cout << CurNumber << std::endl; break;}
