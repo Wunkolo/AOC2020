9b89f9b3fc366ae7634553958bce852e69b13563
Wunkolo <Wunkolo@gmail.com>
2020-12-16 15:07:18 -0800

    Optimize Day 16 solution with bit-arithmetic

--- Day 16/main.cpp
+++ Day 16/main.cpp
@@ -6,6 +6,7 @@
 #include <cassert>
 #include <map>
 #include <set>
+#include <bit>
 
 struct Rule
 {
@@ -20,20 +21,20 @@ struct Rule
 
 int main()
 {
-	// FieldStart->FieldEnd | FieldStart->Ticket
+	// Rule Index -> Rule
 	std::map<std::uintmax_t, Rule> Rules;
+	// RuleMinA -> Rule*, RuleMinB -> Rule*
+	// Used for quickly looking up a number and the rule it lands on via lower_bound
 	std::map<std::uintmax_t, Rule*> RuleSpans;
-	std::set<std::uintmax_t> ValidFields, DepartureField;
+
+	// Interpret rules
 	char Name[256];
 	std::uintmax_t MinA, MinB, MaxA, MaxB;
-
 	std::string CurLine;
 	std::uintmax_t RuleIdx = 0;
 	while( std::getline(std::cin, CurLine) && CurLine.size() )
 	{
-		if(
-			std::sscanf( CurLine.c_str(), " %[^:]: %ju-%ju or %ju-%ju", Name, &MinA, &MaxA, &MinB, &MaxB) == 5
-		)
+		if( std::sscanf( CurLine.c_str(), " %[^:]: %ju-%ju or %ju-%ju", Name, &MinA, &MaxA, &MinB, &MaxB) == 5 )
 		{
 			Rule CurRule;
 			CurRule.MinA = MinA; CurRule.MaxA = MaxA;
@@ -43,6 +44,7 @@ int main()
 			++RuleIdx;
 		}
 	}
+	assert(Rules.size() == 20);
 
 	// Your Ticket
 	std::vector<std::uintmax_t> YourTicket;
@@ -51,6 +53,7 @@ int main()
 	{
 		std::stringstream TicketStream(CurLine); std::string CurNum;
 		while(std::getline(TicketStream, CurNum, ',')) YourTicket.push_back(std::stoull(CurNum));
+		assert(YourTicket.size() == 20);
 	}
 
 	// Nearby tickets
@@ -63,8 +66,10 @@ int main()
 		auto& CurTicket = NearbyTickets.back();
 		std::stringstream TicketStream(CurLine); std::string CurNum;
 		while(std::getline(TicketStream, CurNum, ',')) CurTicket.push_back(std::stoull(CurNum));
+		assert(CurTicket.size() == 20);
 	}
 
+	//// Part 1
 	std::uintmax_t Part1{};
 	std::vector<std::vector<std::uintmax_t>> ValidTickets;
 	for(const auto& CurTicket : NearbyTickets)
@@ -72,8 +77,8 @@ int main()
 		bool ValidTicket = true;
 		for(auto& Num : CurTicket)
 		{
-			const auto Match = --RuleSpans.lower_bound(Num);
-			if(!Match->second->Test(Num))
+			auto Match = RuleSpans.lower_bound(Num);
+			if(Match == RuleSpans.begin() || !(--Match)->second->Test(Num))
 			{
 				// Not matched
 				Part1 += Num;
@@ -85,7 +90,12 @@ int main()
 			ValidTickets.push_back(CurTicket);
 		}
 	}
-	std::vector<std::vector<std::uintmax_t>> Columns(ValidTickets[0].size());
+	std::cout << Part1 << std::endl;
+
+	//// Part 2
+	// Create a transposed matrix of the valid tickets
+	// Each column-vector in columns is all of the other ticket's first number
+	std::vector<std::vector<std::uintmax_t>> Columns(20);
 
 	for( std::size_t Row = 0; Row < ValidTickets.size(); ++Row )
 	{
@@ -94,50 +104,44 @@ int main()
 			Columns[Column].push_back(ValidTickets[Row][Column]);
 		}
 	}
-	std::map<std::uintmax_t, std::set<std::uintmax_t>> Possibilities;
-
+	// Test each input column against each rule. If a whole column passes a rule
+	// then add it to the list of possibilities
+	std::vector<std::uint32_t> Possibilities(Columns.size());
 	for( std::size_t i = 0; i < Columns.size(); ++i )
 	{
 		for(const auto Span : Rules)
 		{
 			bool ValidCol = true;
 			for(const auto Val : Columns[i]) ValidCol &= Span.second.Test(Val);
-			if(ValidCol) Possibilities[i].insert(Span.first);
+			if(ValidCol) Possibilities[i] |= (1u << Span.first);
 		}
 	}
 
-	std::vector<std::uintmax_t> OrderIndex(Possibilities.size());\
+	std::vector<std::uintmax_t> OrderIndex(Possibilities.size());
 	// Gaussian elimination
 	while(Possibilities.size())
 	{
-		// Find the column with the least amount of possibilities
-		std::uintmax_t RuleIndex = ~0ull;
-		std::uintmax_t MinColumnSize = ~0ull;
-		for( auto Possibility : Possibilities )
-		{
-			if(Possibility.second.size() < MinColumnSize)
-			{
-				RuleIndex = Possibility.first;
-				MinColumnSize = Possibility.second.size();
-			}
-		}
-		// This _should_ be a set of just 1 element each time, otherwise this
-		// is not a solvable system
-		assert(Possibilities[RuleIndex].size() == 1);
+		// Find the column with the least amount of possibilities(just 1 element)
+		const auto MinRule = std::find_if(
+			Possibilities.cbegin(), Possibilities.cend(),
+			std::has_single_bit<std::uint32_t>
+		);
+		if(MinRule == Possibilities.cend()) break;
+		const auto MinRuleIndex = std::distance(Possibilities.cbegin(), MinRule);
+		const std::uint32_t CurBasis = *MinRule;
 		// We get this [ ... 0 0 0 1 0 0 0 ... ] vector and remove it from
 		// from the possibilities
-		const std::uintmax_t CurBasis = *Possibilities[RuleIndex].begin();
-		Possibilities.erase(RuleIndex);
+		// Remove this column from further testing, we know what it is
+		//Possibilities.erase(Possibilities.begin() + MinRuleIndex);
 		// Add it to the order, now that we know exactly where this one should be
-		OrderIndex[CurBasis] = RuleIndex;
+		OrderIndex[std::countr_zero(CurBasis)] = MinRuleIndex;
 		// Remove this possibility from all other possibility-sets
-		for( auto& Possibility : Possibilities ) Possibility.second.erase(CurBasis);
+		for( auto& Possibility : Possibilities ) Possibility &= ~CurBasis;
 	}
 	std::uintmax_t Part2 = 1;
 	for( std::size_t i = 0; i < 6; ++i )
 	{
 		Part2 *= YourTicket[OrderIndex[i]];
 	}
-	std::cout << Part1 << std::endl;
 	std::cout << Part2 << std::endl;
 }
