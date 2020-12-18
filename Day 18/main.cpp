7b5bede886b122b4bc53379eaf68a7d544c98561
Wunkolo <Wunkolo@gmail.com>
2020-12-17 22:49:24 -0800

    Refactor Day 18 Solution

--- Day 18/main.cpp
+++ Day 18/main.cpp
@@ -1,53 +1,17 @@
 #include <iostream>
 #include <cstdio>
 #include <vector>
+#include <numeric>
 #include <cctype>
 #include <stack>
 #include <queue>
 
-std::uintmax_t Evaluate(const std::vector<char>& Expression, bool Part2 = false)
+template< bool Part2 = false >
+std::uintmax_t Evaluate(const std::vector<char>& Expression)
 {
 	std::stack<char> OperatorStack;
 	std::stack<std::uintmax_t> OperandStack;
-	for(const auto& Token : Expression)
-	{
-		if( std::isdigit(Token) ) OperandStack.push(Token - '0');
-		else if( Token == '(') OperatorStack.push(Token);
-		else if( Token == ')')
-		{
-			while(OperatorStack.size() && OperatorStack.top() != '(')
-			{
-				const auto OperandA = OperandStack.top(); OperandStack.pop();
-				const auto OperandB = OperandStack.top(); OperandStack.pop();
-				switch(OperatorStack.top())
-				{
-					case '+': OperandStack.push(OperandA + OperandB); break;
-					case '*': OperandStack.push(OperandA * OperandB); break;
-				}
-				OperatorStack.pop();
-			}
-			if(OperatorStack.top() == '(') OperatorStack.pop();
-		}
-		else if( Token == '+' || Token == '*' )
-		{
-			while(
-				OperatorStack.size() && OperatorStack.top() != '('
-				&& (!Part2 || (Part2 && Token == '*' && OperatorStack.top() == '+'))
-			)
-			{
-				const auto OperandA = OperandStack.top(); OperandStack.pop();
-				const auto OperandB = OperandStack.top(); OperandStack.pop();
-				switch(OperatorStack.top())
-				{
-					case '+': OperandStack.push(OperandA + OperandB); break;
-					case '*': OperandStack.push(OperandA * OperandB); break;
-				}
-				OperatorStack.pop();
-			}
-			OperatorStack.push(Token);
-		}
-	}
-	while(OperatorStack.size())
+	const auto EvalTerm = [&OperatorStack, &OperandStack]()
 	{
 		const auto OperandA = OperandStack.top(); OperandStack.pop();
 		const auto OperandB = OperandStack.top(); OperandStack.pop();
@@ -57,7 +21,33 @@ std::uintmax_t Evaluate(const std::vector<char>& Expression, bool Part2 = false)
 			case '*': OperandStack.push(OperandA * OperandB); break;
 		}
 		OperatorStack.pop();
+	};
+	for(const auto& Token : Expression)
+	{
+		switch(Token)
+		{
+			case '(': OperatorStack.push(Token); break;
+			case ')':
+			{
+				while(OperatorStack.size() && OperatorStack.top() != '(') EvalTerm();
+				if(OperatorStack.top() == '(') OperatorStack.pop();
+				break;
+			}
+			case '+':
+			case '*':
+			{
+				while(
+					OperatorStack.size() && OperatorStack.top() != '('
+					&& (!Part2 || (Part2 && Token == '*' && OperatorStack.top() == '+'))
+				) EvalTerm();
+				OperatorStack.push(Token);
+				break;
+			}
+			default:
+				if( std::isdigit(Token) ) OperandStack.push(Token - '0'); break;
+		}
 	}
+	while(OperatorStack.size()) EvalTerm();
 	return OperandStack.top();
 }
 
@@ -75,11 +65,13 @@ int main()
 		Expressions.push_back(std::move(CurExpression));
 	}
 
-	std::uintmax_t Part1{};
-	for(const auto& CurExpression : Expressions) Part1 += Evaluate(CurExpression);
-	std::cout << Part1 << std::endl;
+	std::cout << std::transform_reduce(
+		Expressions.cbegin(), Expressions.cend(),
+		0ull, std::plus<>(), Evaluate<false>
+	) << std::endl;
 
-	std::uintmax_t Part2{};
-	for(const auto& CurExpression : Expressions) Part2 += Evaluate(CurExpression, true);
-	std::cout << Part2 << std::endl;
+	std::cout << std::transform_reduce(
+		Expressions.cbegin(), Expressions.cend(),
+		0ull, std::plus<>(), Evaluate<true>
+	) << std::endl;
 }
